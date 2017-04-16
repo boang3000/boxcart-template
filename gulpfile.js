808f9977907318ec8939043573834fce43833164
Rodel Amancio <rodelmarkamancio@gmail.com>
2017-04-16 22:50:09 +0800

    updated front-end config file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
+    // sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
     notify = require("gulp-notify"),
     bower = require('gulp-bower');
 
 var config = {
-    sassPath: '/assets/sass',
-    bowerDir: '/bower_components'
+    sassPath: './assets/sass',
+    npmDir: './node_modules',
+    bowerDir: './bower_components'
 }
 
 gulp.task('bower', function() {
@@ -18,16 +20,26 @@ gulp.task('icons', function() {
         .pipe(gulp.dest('./dist/fonts'));
 });
 
+// gulp.task('css', function () {
+//     return gulp.src(config.sassPath + '/main.scss')
+//         .pipe(sass({
+//             style: 'compressed',
+//             loadPath: [
+//                 './resources/sass',
+//                 config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+//                 config.bowerDir + '/fontawesome/scss/*',
+//             ]
+//         }))
+//         .pipe(gulp.dest('./dist/css'));
+// });
 gulp.task('css', function () {
-    return gulp.src(config.sassPath + '/main.scss')
-        .pipe(sass({
-            style: 'compressed',
-            loadPath: [
-                './resources/sass',
-                config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
-                config.bowerDir + '/fontawesome/scss',
-            ]
-        }))
+    gulp.src([
+            config.sassPath + '/main.scss', 
+            config.sassPath + '/**/*.scss',
+            config.npmDir + '/bootstrap-sass/assets/stylesheets/**.scss',
+            config.npmDir + '/fontawesome/scss/*',
+        ])
+        .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 });
 
