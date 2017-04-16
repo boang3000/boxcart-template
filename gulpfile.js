9bdd432326860bfcd951ff20868eac7bd477a6ef
Rodel Amancio <rodelmarkamancio@gmail.com>
2017-04-16 23:46:27 +0800

    updated config for front-end and assets ready

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ var gulp = require('gulp'),
     // sass = require('gulp-ruby-sass'),
     sass = require('gulp-sass'),
     notify = require("gulp-notify"),
-    bower = require('gulp-bower');
+    bower = require('gulp-bower'),
+    imagemin = require('gulp-imagemin'),
+    image = require('gulp-image')
+    path = require('path');
 
 var config = {
     sassPath: './assets/sass',
     npmDir: './node_modules',
-    bowerDir: './bower_components'
+    bowerDir: './bower_components',
+    imgPath: './assets/images'
 }
 
 gulp.task('bower', function() {
@@ -20,18 +24,6 @@ gulp.task('icons', function() {
         .pipe(gulp.dest('./dist/fonts'));
 });
 
-// gulp.task('css', function () {
-//     return gulp.src(config.sassPath + '/main.scss')
-//         .pipe(sass({
-//             style: 'compressed',
-//             loadPath: [
-//                 './resources/sass',
-//                 config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
-//                 config.bowerDir + '/fontawesome/scss/*',
-//             ]
-//         }))
-//         .pipe(gulp.dest('./dist/css'));
-// });
 gulp.task('css', function () {
     gulp.src([
             config.sassPath + '/main.scss', 
@@ -43,9 +35,20 @@ gulp.task('css', function () {
         .pipe(gulp.dest('./dist/css'));
 });
 
+// gulp.task('images', function() {
+//   return gulp.src(config.imgPath + '/images/*.+(png|jpg|gif|svg)')
+//     .pipe(imagemin())
+//     .pipe(gulp.dest('./dist/images'))
+// });
+gulp.task('images', function () {
+    return gulp.src(config.imgPath + '/*.+(png|jpg|gif|svg)')
+    // .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest(path.join('./dist/', 'images')))
+});
+
 // Rerun the task when a file changes
 gulp.task('watch', function() {
     gulp.watch(config.sassPath + '/**/*.scss', ['css']);
 });
 
-gulp.task('default', ['bower', 'icons', 'css']);
+gulp.task('default', ['bower', 'icons', 'css', 'images']);
